2f600b2f29cd95d520397374911c00fcd0ed9836
Bluchip6 <mildseason3ees@gmail.com>
2020-12-21 10:53:47 +0000

    me

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,8 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("musicstream.c7apijsslqfc.us-east-1.rds.amazonaws.com", "abdul", "12345678", "rauph_hussle");
+	$con = mysqli_connect("musicstream.c7apijsslqfc.us-east-1.rds.amazonaws.com", "abdul", "12345678", "slotify
+	");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
